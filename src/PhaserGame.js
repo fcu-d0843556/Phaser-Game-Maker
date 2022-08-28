35fc03a0ca2369cf01d80d1ce2e1ae20dd00a05e
fcu-d0843556 <kevinfinity@mail.fcu.edu.tw>
2022-08-28 16:17:57 +0800

    Init react phaser

---
+++ src/PhaserGame.js
@@ -0,0 +1,36 @@
+import Phaser from 'phaser'
+
+import HelloWorldScene from './scenes/HelloWorldScene.ts'
+
+// const config = {
+// 	type: Phaser.CANVAS,
+// 	parent: 'phaser-container',
+// 	width:  window.innerWidth * window.devicePixelRatio, //800
+// 	height: window.innerHeight * window.devicePixelRatio, //600
+// 	backgroundColor: '#000111',
+// 	dom: {
+// 		createContainer: true,
+// 	},
+// 	scale:{
+// 		//autoCenter: Phaser.Scale.CENTER_BOTH,
+// 		// mode:Phaser.Scale.RESIZE,
+// 	},
+// 	physics: {
+// 		default: 'arcade',
+// 		arcade: {
+// 		gravity: { x: 0, y: 0 },
+
+// 		},
+// 	},
+// 	scene: [HelloWorldScene],
+// 	// scene: {
+// 	// 	pack: {
+// 	// 		files: [
+// 	// 			{ type: 'image', key: 'foodCan', url: `src/static/upload/${userPath}/btn_login_hover.png` },
+// 	// 		]
+// 	// 	}
+// 	// }
+// }
+
+// const game = new Phaser.Game(config);
+
