fde5d8ec83c9199ab15db63da85c2b6b8c42e4f5
fcu-d0843556 <kevinfinity@mail.fcu.edu.tw>
2022-09-04 19:55:46 +0800

    Remove react strict mode to fix the bug the componentdidmount run twice.

--- src/PhaserGame.js
+++ src/PhaserGame.js
@@ -2,35 +2,41 @@ import Phaser from 'phaser'
 
 import HelloWorldScene from './scenes/HelloWorldScene.ts'
 
-// const config = {
-// 	type: Phaser.CANVAS,
-// 	parent: 'phaser-container',
-// 	width:  window.innerWidth * window.devicePixelRatio, //800
-// 	height: window.innerHeight * window.devicePixelRatio, //600
-// 	backgroundColor: '#000111',
-// 	dom: {
-// 		createContainer: true,
-// 	},
-// 	scale:{
-// 		//autoCenter: Phaser.Scale.CENTER_BOTH,
-// 		// mode:Phaser.Scale.RESIZE,
-// 	},
-// 	physics: {
-// 		default: 'arcade',
-// 		arcade: {
-// 		gravity: { x: 0, y: 0 },
+// eslint-disable-next-line import/no-anonymous-default-export
+export const config = {
+	type: Phaser.AUTO,
+	// type: Phaser.CANVAS,
+	parent: 'phaser-container',
+	width:  360, //800
+	height: 640, //600
+	// width:  window.innerWidth * window.devicePixelRatio, //800
+	// height: window.innerHeight * window.devicePixelRatio, //600
+	backgroundColor: '#000111',
+	dom: {
+		createContainer: true,
+	},
+	scale:{
+		//autoCenter: Phaser.Scale.CENTER_BOTH,
+		// mode:Phaser.Scale.RESIZE,
+	},
+	physics: {
+		default: 'arcade',
+		arcade: {
+		gravity: { x: 0, y: 0 },
 
-// 		},
-// 	},
-// 	scene: [HelloWorldScene],
-// 	// scene: {
-// 	// 	pack: {
-// 	// 		files: [
-// 	// 			{ type: 'image', key: 'foodCan', url: `src/static/upload/${userPath}/btn_login_hover.png` },
-// 	// 		]
-// 	// 	}
-// 	// }
-// }
+		},
+	},
+	scene: [HelloWorldScene],
+	// scene: {
+	// 	pack: {
+	// 		files: [
+	// 			{ type: 'image', key: 'foodCan', url: `src/static/upload/${userPath}/btn_login_hover.png` },
+	// 		]
+	// 	}
+	// }
+};
 
-// const game = new Phaser.Game(config);
+export default function startGame(){
+	const game = new Phaser.Game(config)
 
+}
--- src/pages/GameMaker/GameMaker.jsx
+++ src/pages/GameMaker/GameMaker.jsx
@@ -4,7 +4,15 @@ import React, { Component } from 'react'
 import GameScreen from './GameScreen/GameScreen'
 import EditScreen from './EditScreen/EditScreen'
 
+import startGame from '../../PhaserGame'
+
 export default class GameMaker extends Component {
+
+  componentDidMount(){
+    console.log('11');
+    startGame()
+  }
+
   render() {
     return (
         <div className="container-fluid">
