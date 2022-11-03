c61358937f1722577fdcd8df9c7c51dc443a86c1
fcu-d0843556 <kevinfinity@mail.fcu.edu.tw>
2022-11-03 15:54:50 +0800

    Modify login jsx style - still developing.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import PubSub from 'pubsub-js'
-import {Button, Form, Input} from 'antd'
+import { Form, Input,Row,Col,Layout} from 'antd'
+import { Button, Card, InputNumber,Tooltip, Space, message,Alert,Divider } from 'antd';
+import { QuestionCircleTwoTone,UserOutlined,LockOutlined } from '@ant-design/icons';
+
 import axios from 'axios'
 
 //Components
@@ -12,6 +15,8 @@ import LineLogin from './LineLogin/LineLogin'
 
 //css
 import './Login.css'
+
+const { Content } = Layout;
 
 export default class Login extends Component {
   render() {
@@ -45,63 +50,100 @@ export default class Login extends Component {
     };
 
     return (
-      <div className="loginBox">
-          <div className="loginTitle">登入</div>
-          <br/><br/>
+        <Row>
+              {/* 讓BMIIntro這個組件可以顯示在左半邊（BMIIntro是顯示4個結果的頁面） */}
+              <Col span={12}>
+                  <Content
+                      style={{
+                      padding: '0 50px',
+                      height: "100%",
+                      backgroundColor:"#3AAFA9"
+                      }}
+                  >
+                    <Row type="flex" justify="center" align="middle" style={{minHeight: '100vh'}}></Row>
+                      {/* <BMIIntro></BMIIntro> */}
+                  </Content>
+              </Col>
 
-        <Form
-          name="basic"
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="on"
-        >
-          <Form.Item
-            className="textbar"
-            label="用戶名" name="username"
-            rules={[
-              {
-                required: true,
-                message: '請輸入用戶名！',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
+              {/* 讓BMIForm這個組件可以顯示在右半邊（BMIForm是使用者輸入BMI資料的頁面） */}
+              <Col span={12}>
+                  <Content
+                      style={{
+                      padding: '0 50px',
+                      height: "100%",
+                      }}
+                  >
+                      <Row type="flex" justify="center" align="middle" style={{minHeight: '100vh'}}>
+                          <Col span={16}>
+                              <Card
+                                  title="BMI 計算器"
+                                  headStyle={{borderBottom: "1px solid #545454"}}
+                                  style={{
+                                      width: "auto",
+                                      height: "auto",
+                                      border: "1px solid black"
+                                  }}
+                              >
+                                  <Form
+                                      style={{backgroundColor: "white"}}
+                                      onFinish={onFinish}
+                                      onFinishFailed={onFinishFailed}
+                                      autoComplete="on"
+                                  >
 
-          <Form.Item
-            className="textbar"
-            label="密碼" name="password"
-            rules={[
-              {
-                required: true,
-                message: '請輸入密碼！',
-              },
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
+                                      <Form.Item
+                                        name="username"
+                                        rules={[
+                                          {
+                                            required: true,
+                                            message: '請輸入使用者名稱！',
+                                          },
+                                        ]}
+                                      >
+                                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="使用者名稱" />
+                                      </Form.Item>
 
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              登入
-            </Button>
-          </Form.Item>
+                                      <Form.Item
+                                        name="password"
+                                        rules={[
+                                          {
+                                            required: true,
+                                            message: '請輸入密碼！',
+                                          },
+                                        ]}
+                                      >
+                                        <Input
+                                          prefix={<LockOutlined className="site-form-item-icon" />}
+                                          type="password"
+                                          placeholder="密碼"
+                                        />
+                                      </Form.Item>
+                          
 
-          <Form.Item>
-            <MyNavLink to="/register">
-              <Button type="primary" htmlType="submit">
-                註冊
-              </Button>
-            </MyNavLink>
-            <br/><br/><br/>
-          </Form.Item>
-        </Form>
+                                      <Divider></Divider>
 
-        {/* <LineLogin></LineLogin> */}
-      </div>
+                                      <Form.Item>
+                                        <Button type="primary" htmlType="submit" className="login-form-button" block>
+                                          登入
+                                        </Button>
+
+                                        
+                                      </Form.Item>
+
+                                      <Form.Item>
+                                        <MyNavLink to="/register">
+                                          <Button type="primary" htmlType="submit" block>
+                                            註冊
+                                          </Button>
+                                        </MyNavLink>
+                                      </Form.Item>
+                                  </Form>
+                              </Card>
+                          </Col>
+                      </Row>
+                  </Content>
+              </Col>
+          </Row>
     )
   }
 }
