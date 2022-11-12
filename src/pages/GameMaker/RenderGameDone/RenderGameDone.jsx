cddb78085190dd461d00ed4c58640272b156ccbf
fcu-d0843556 <kevinfinity@mail.fcu.edu.tw>
2022-11-12 16:52:43 +0800

    RenderGameDone new style have completed.

--- src/pages/GameMaker/RenderGameDone/RenderGameDone.jsx
+++ src/pages/GameMaker/RenderGameDone/RenderGameDone.jsx
@@ -1,58 +1,80 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom';
 
-import {Typography} from 'antd'
+import {Typography, Result, Button,Modal} from 'antd'
+import { SmileTwoTone } from '@ant-design/icons';
 
-const { Paragraph } = Typography;
+
+
+import './RenderGameDone.less'
+
+const { Paragraph, Title} = Typography;
 
 class RenderGameDone extends Component {
 
+    state = {
+        isModalOpen: false
+    }
 
     componentDidMount(){
         const {autoClick} = this
         autoClick.click()
     }
 
-    goToPlayGame = () => {
-        const {continueModifyButton} = this
-        const {gameUrl} = this.props    
-        continueModifyButton.click()
-        this.props.history.replace(gameUrl)
-    }
-
     render() {
-
+        const {isModalOpen} = this.state
         const {gameUrl} = this.props
+
+        const showModal = () => {
+            this.setState({isModalOpen: true})
+        };
+
+        const goToPlayGame = () => {
+            const {gameUrl} = this.props    
+            this.props.history.replace(gameUrl)
+        }
+
+        const handleCancel = () => {
+            this.setState({isModalOpen: false})
+        };
 
         return (
             <div>
-                <div className="modal fade" id="staticBackdrop" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg ">
-                        <div className="modal-content" style={{"backgroundColor": "rgb(235, 152, 0)"}}>
-                            <div className="modal-header">
-                                <h2 className="modal-title" id="staticBackdropLabel">完 成！</h2>
-                            </div>
-                        
-                            <div className="modal-body"  style={{backgroundColor: "orange"}}>
-                                <div className="card-body">
-                                    <h4 className="card-text">您已經完成遊戲的製作了</h4>
-                                    <h4 className="card-text">趕快分享給對方遊玩吧！！</h4>
-                                    <div className="mb-3">
-                                        <h4 htmlFor="disabledTextInput" className="form-label" >這個是生成出來的鏈接：</h4>
-                                        <Paragraph className="form-control" copyable>{"http://140.134.26.66:5051/" + gameUrl}</Paragraph>
-                                    </div>
-                                </div>
-                            </div>
+                {/* 自動點擊按鈕不顯示 */}
+                <Button ref={c => this.autoClick = c} onClick={showModal} style={{display: "none"}}/>
 
-                            <div className="modal-footer">
-                                <button ref={c => this.continueModifyButton = c} type="button" className="btn btn-secondary" data-bs-dismiss="modal">繼續編輯</button>
-                                <button onClick={this.goToPlayGame} type="button" className="btn btn-primary">前往遊玩網頁</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                {/* 遊戲完成視窗 */}
+                <Modal bodyStyle={{background: "#F7E9DF"}}  
+                       width={700} 
+                       style={{top: "10%"}} 
+                       title= {<Title level={3} style={{margin: 0}} className="render-game-done-title">製 作 完 成！</Title>}
+                       open={isModalOpen} 
+                       onCancel={handleCancel}
+                       footer={[
+                        <Button key="continue" className='render-game-done-continue-modify-button' onClick={handleCancel}> 繼續編輯 </Button>,
+                        <Button key="playGame" className='render-game-done-go-to-playgame-button' onClick={goToPlayGame}> 前往遊玩網頁</Button>
+                      ]}
+                >
 
-                <button ref={c => this.autoClick = c} style={{display: "none"}} type="button" id="autoClick" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop"/>
+                    {/* 內容顯示區 */}
+                    <Result
+                        icon={<SmileTwoTone twoToneColor="#538CF6" />}
+                        status="success"
+                        title= {<Title level={2} >您已經完成遊戲的製作</Title>}
+                        subTitle={<Title level={3} >趕快分享給對方遊玩吧！</Title>}
+                    />
+                    
+                    {/* 文字鏈接區 */}
+                    <Typography>
+                        <Title level={3} >以下是生成出來的鏈接 :</Title>
+                        <pre>
+                            <Paragraph copyable>
+                                {"http://140.134.26.66:5051/" + gameUrl}
+                            </Paragraph>
+                        </pre>
+                    </Typography>
+
+                </Modal>
             </div>
         )
     }
