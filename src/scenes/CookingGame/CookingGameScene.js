4d254dbe4a14c798275c2864e38fa599b6808853
fcu-d0843556 <kevinfinity@mail.fcu.edu.tw>
2022-11-21 17:05:36 +0800

    Remove unnecessary files and function in cookingGameScene.

--- src/scenes/CookingGame/CookingGameScene.js
+++ src/scenes/CookingGame/CookingGameScene.js
@@ -4,102 +4,84 @@ import GameTimer from "../CommonSystem/GameTimer"
 import Score from "../CommonSystem/Score"
 import GameoverMessage from "../CommonSystem/GameOverMessage"
 
+
+//Cooking Game Scripts
 import MaskManSpawner from "./MaskManSpawner"
 import FoodSpawner from "./FoodSpawner"
-
-
+import GameTutorial from "./GameTutorial"
 
 var zone
 
 export default class CookingGameScene extends Phaser.Scene{
-    constructor(userID,appSpot){
+    constructor(){
         super('Cooking')
-        this.userID = userID
-        this.appSpot = appSpot
-
         this.food = undefined
-        this.allJsonData = []
         this.scoreText = undefined
-        this.gameOver = false
+        this.isGameStart = false
     }
 
     preload(){
-        // this.jsonData = this.cache.json.get('jsonData');
-        // console.log("find jsonData")
-        // console.log(this.jsonData)
+        this.modifyDatas = this.scene.settings.data
+        console.log("modifyDatas : ", this.modifyDatas)
 
-        this.load.image('half','/img/Games/CookingGame/halfFood.png')
-        this.load.image('flip','/img/Games/CookingGame/halfFlipFood.png')
-        this.load.image('well','/img/Games/CookingGame/wellFood.png')
+        //load image
+        Object.keys(this.modifyDatas).forEach((key)=>{
+            this.modifyDatas[key].items.forEach((itemObj)=>{
+                if( itemObj.img ) {
+                    if(this.textures.list[itemObj.name]){
+                        // console.log("remove");
+                        this.textures.remove(itemObj.name)
+                    }
+                    // console.log("add", itemObj);
+                    this.load.image( itemObj.name, itemObj.img.src )
+                }
+            })
+        })
+
+
+        this.load.image('startGameLabel','/img/Games/Common/gameover/startGameLabel.png')
+        this.load.image('startGameButton', '/img/Games/Common/gameover/startGameButton.png')
+        this.load.image('gameoverLabel','/img/Games/Common/gameover/gameoverLabel.png')
+        this.load.image('playAgainButton', '/img/Games/Common/gameover/playAgainButton.png')
+
+
         this.load.image('panel','/img/Games/CookingGame/cookpanel.png')
         this.load.image('blackBlock','/img/Games/CookingGame/blackBlock.png')
 
+
+
         this.load.image('foodCan','/img/Games/CookingGame/foodCan.png')
         this.load.image('rawFood','/img/Games/CookingGame/rawFood.png')
-        this.load.image('background','/img/Games/Common/background.png')
-
+        this.load.image('half','/img/Games/CookingGame/halfFood.png')
+        this.load.image('flip','/img/Games/CookingGame/halfFlipFood.png')
+        this.load.image('well','/img/Games/CookingGame/wellFood.png')
         this.load.image('cookSpot','/img/Games/CookingGame/cookSpot.png')
+
+
+
+
+
+
+        
         this.load.image('maskMan','/img/Games/CookingGame/maskMan.png')
         this.load.image('maskManSmile','/img/Games/CookingGame/maskManSmile.png')
         this.load.image('maskManAngry','/img/Games/CookingGame/maskManAngry.png')
-
         this.load.image('wantedItem','/img/Games/CookingGame/wantedItem.png')
-
-
-        this.load.image('gameover','/img/Games/Common/gameover/gameoverLabel.png')
-        this.load.image('playAgain', '/img/Games/Common/gameover/playAgainButton.png')
-
-        // const items = this.jsonData.items
-        // console.log("all items = ")
-        // console.log(items)
-
-        // for(let t=0; t<items.length; t++){
-        //     if(items[t].items){
-        //         for(let s=0; s<items[t].items.length; s++){
-        //             this.load.image(items[t].items[s].name, this.appSpot + items[t].items[s].src)
-        //         }
-        //         this.allJsonData[items[t].name] = items[t]
-        //     }else{
-        //         if(items[t].src){
-        //             this.load.image(items[t].name, this.appSpot + items[t].src)
-
-        //         }
-        //         this.allJsonData[items[t].name] = items[t]
-        //     }
-            
-        // }
-
-        // console.log("all allJsonData = ")
-        // console.log(this.allJsonData)
     }
 
 
     create(){
 
-        this.add.image(400,320 ,'background').setScale(1)
+        //background custom OK.
+        const {background} = this.modifyDatas
+        this.add.image(background.items[0].img.position.x, background.items[0].img.position.y ,'background').setScale(background.items[0].img.size/100)
 
-        // this.add.image(this.allJsonData.background.position.x, this.allJsonData.background.position.y ,'background').setScale(this.allJsonData.background.size/100)
+
         this.add.image(400,480,'panel').setDepth(1);
         this.add.image(400,620,'blackBlock')
 
-
-
-        const scoreTextLabel = this.add.text(16,16, "score", {
-            "fontSize": 32,
-            "fill": "#000"
-        })
-        this.scoreText = new Score(this,scoreTextLabel,"score",0)
-        this.scoreText.showScoreText()
-
-
-
-        const timerLabel2 = this.add.text(16, 54, "time", {
-            "fontSize": 32,
-            "fill": "#000"
-        })
-        this.gameTimer = new GameTimer(this,timerLabel2,"time")
-        this.gameTimer.start(this.gameover.bind(this),3000)//5s
-
+        //create score
+        this.createScoreBoard()
 
         
 
@@ -134,7 +116,10 @@ export default class CookingGameScene extends Phaser.Scene{
 
         this.setInputInteractive()
         
-        
+        //gameStart Tutorial
+        const {gameTutorialText} = this.modifyDatas
+        this.gameTutorialMessage = new GameTutorial(this, gameTutorialText.items[0])
+        this.gameTutorialMessage.create()
     }
 
     setInputInteractive(){
@@ -178,10 +163,9 @@ export default class CookingGameScene extends Phaser.Scene{
     }
     gameover(){
         this.physics.pause()
-        this.gameOver = true
-
-        let gameoverMessage = new GameoverMessage(this,this.scoreText.getScore())
-        gameoverMessage.create()
+        const {gameoverMessage} = this.modifyDatas
+        this.gameoverMessage = new GameoverMessage(this,this.scoreText.getScore(),gameoverMessage.items[0])
+        this.gameoverMessage.create()
     }
 
     createFoodSpot(){
@@ -215,8 +199,44 @@ export default class CookingGameScene extends Phaser.Scene{
             this.foodGroup[i].timer.update()
         }
         zone.body.debugBodyColor = zone.body.touching.none ? 0x00ffff : 0xffff00
-        this.gameTimer.update()
 
+        if(this.isGameStart){
+            this.gameTimer.update()
+        }
+    }
+
+    startGame = () => {
+        //create score board
+        this.scoreText.showScoreText()
+        
+
+        // timeText,gameTimer custom OK.
+        const {gameTimer} = this.modifyDatas
+        this.createGameTimer(gameTimer.items[0])
+    }
+
+    createGameTimer(gameTimer){
+        //Timer
+        const style = {
+            fontSize: 32,
+            fill: "#fff",
+            stroke: "#000",
+            strokeThickness: 2
+        }
+        const gameTimerLabel = this.add.text(16, 34, "時間", style)
+        this.gameTimer = new GameTimer(this, gameTimerLabel, "\n時間")
+        this.gameTimer.start(this.gameover.bind(this),gameTimer.text.content * 1000)//5s
+    }
+
+    createScoreBoard(){
+        //Score
+        const scoreTextLabel = this.add.text(16,-10, "", {
+            "fontSize": 32,
+            "fill": "#fff",
+            "stroke": "#000",
+            "strokeThickness": 2
+        })
+        this.scoreText = new Score(this,scoreTextLabel,"\n分數",0)
     }
 }
 
