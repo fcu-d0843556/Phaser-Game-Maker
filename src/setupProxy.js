137357e41734dec574800583d0d855cea101fb58
fcu-d0843556 <kevinfinity@mail.fcu.edu.tw>
2022-12-04 23:19:22 +0800

    Using deploy location.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -58,8 +58,8 @@ module.exports = function(app) {
   app.use(
     '/api1', //遇見api1前綴的請求，就會觸發該代理配置
     createProxyMiddleware({
-      // target: 'http://140.134.26.66:5050', //請求轉發給誰
-      target: 'http://localhost:5050', //請求轉發給誰
+      target: 'http://140.134.26.66:5050', //請求轉發給誰
+      // target: 'http://localhost:5050', //請求轉發給誰
       changeOrigin: true,              //控制服務器收到的請求頭中Host字段的值
       pathRewrite:{'^/api1':''}        //重寫請求路徑
     })
